8047e66d78e68bf50cb35280770c12cd62d10738
hallsarturo <proalarturo@gmail.com>
2026-01-08 12:31:38 -0600

    Implement mergeTwoLists function to merge two sorted linked lists

--- leetcode/easy-medium/linked-lists/21-merge-two-sorted-lists.js
+++ leetcode/easy-medium/linked-lists/21-merge-two-sorted-lists.js
@@ -4,3 +4,56 @@
 
 // Return the head of the merged linked list.
 
+import {
+    LinkedList,
+    LinkedListNode,
+} from '../../book-loiane/linked-list/singly-linked-list.js';
+
+const list1 = new LinkedList();
+const list2 = new LinkedList();
+
+list1.append(1);
+list1.append(2);
+list1.append(4);
+
+list2.append(1);
+list2.append(3);
+list2.append(4);
+
+const mergeTwoLists = function (list1, list2) {
+    // traverse list1 and list2, and insert the values in order
+
+    if (!list1 && !list2) {
+        return list1;
+    } else if (!list1 && list2) {
+        return list2;
+    } else if (list1 && !list2) {
+        return list1;
+    } else {
+        let currentA = list1;
+        let currentB = list2;
+
+        let newHead = null;
+        let currentMain = null;
+        while (currentA.next !== null && currentB.next !== null) {
+            // compare values between the same list index, order them
+            if (currentA.val <= currentB.val) {
+                if (!newHead) {
+                    newHead = currentA;
+                    newHead.next = currentB;
+                    currentMain = newHead.next;
+                } else {
+                    currentMain.next = currentA;
+                }
+            } else {
+                if (!newHead) {
+                    newHead = currentB;
+                    newHead.next = currentA;
+                } else {
+                    currentMain.next = currentB;
+                }
+            }
+        }
+        return newHead;
+    }
+};
