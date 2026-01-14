5b09da271f82461df883b0f9e4b73d1244804910
hallsarturo <proalarturo@gmail.com>
2026-01-14 10:27:45 -0600

    Refactor noRepeat function to improve logic and update test string for clarity

--- claude/two-pointer-technique /5-window-with-2-pointers.js
+++ claude/two-pointer-technique /5-window-with-2-pointers.js
@@ -1,7 +1,7 @@
 // Window with Two Pointers
 // Find the longest substring without repeating characters
 
-const string = 'abcabcdebb';
+const string = 'abcbcdebb';
 // Output: 3 (substring "abc")
 
 const string2 = 'bbbbb';
@@ -12,25 +12,20 @@ const string3 = 'pwwkew';
 
 function noRepeat(s) {
     let left = 0;
-    let right = 1;
-
-    let maxSet = 0;
-
+    let maxLength = 0;
     const set = new Set();
-    set.add(s[left]);
-    while (right < s.length) {
-        if (!set.has(s[right])) {
-            set.add(s[right]);
-            right++;
-        } else {
-            left = right;
-            if (maxSet < set.size) maxSet = set.size;
-            set.clear();
-            set.add(s[left]);
-            right++;
+
+    for (let right = 0; right < s.length; right++) {
+        while (set.has(s[right])) {
+            set.delete(s[left]);
+            left++;
         }
+
+        set.add(s[right]);
+        maxLength = Math.max(maxLength, right - left + 1);
     }
-    return maxSet;
+
+    return maxLength;
 }
 
 console.log(noRepeat(string3));
