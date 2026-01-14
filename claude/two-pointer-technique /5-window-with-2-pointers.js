2bc334167ae815555cd1b9a36556e9f3de4b34e5
hallsarturo <proalarturo@gmail.com>
2026-01-14 10:14:34 -0600

    Update test cases and input strings for noRepeat function to enhance clarity

--- claude/two-pointer-technique /5-window-with-2-pointers.js
+++ claude/two-pointer-technique /5-window-with-2-pointers.js
@@ -1,26 +1,36 @@
 // Window with Two Pointers
 // Find the longest substring without repeating characters
 
-const string = 'abcabcbb';
+const string = 'abcabcdebb';
 // Output: 3 (substring "abc")
 
-// Input: "bbbbb"
+const string2 = 'bbbbb';
 // Output: 1 (substring "b")
 
-// Input: "pwwkew"
+const string3 = 'pwwkew';
 // Output: 3 (substring "wke")
 
 function noRepeat(s) {
     let left = 0;
     let right = 1;
 
+    let maxSet = 0;
+
     const set = new Set();
     set.add(s[left]);
     while (right < s.length) {
         if (!set.has(s[right])) {
-            set.add();
+            set.add(s[right]);
+            right++;
+        } else {
+            left = right;
+            if (maxSet < set.size) maxSet = set.size;
+            set.clear();
+            set.add(s[left]);
+            right++;
         }
     }
+    return maxSet;
 }
 
-console.log(noRepeat(string));
+console.log(noRepeat(string3));
