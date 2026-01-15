f9047ab71dba2705e759b3c846452869d323b10c
hallsarturo <proalarturo@gmail.com>
2026-01-15 17:17:17 -0600

    Add implementation of moveZeroes function to move all zeros to the end of the array while maintaining order

---
+++ leetcode/easy-medium/283-move-zeroes.js
@@ -0,0 +1,30 @@
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
+// Example 1:
+
+const nums = [0, 1, 0, 3, 12];
+// Output: [1,3,12,0,0]
+// Example 2:
+
+const nums2 = [0];
+// Output: [0]
+
+const moveZeroes = function (nums) {
+    let writePointer = 0;
+    let readPointer = 0;
+
+    while (readPointer < nums.length) {
+        if (nums[readPointer] === 0) {
+            readPointer++;
+        } else {
+            nums[writePointer] = nums[readPointer];
+            writePointer++;
+            readPointer++;
+        }
+    }
+    return nums;
+};
+
+console.log(moveZeroes(nums));
