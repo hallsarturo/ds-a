e8713b1695ce552778ee5d69b916c501aaf18cef
hallsarturo <proalarturo@gmail.com>
2026-01-16 09:14:02 -0600

    Refactor reverseString function to implement in-place reversal without using built-in methods

--- leetcode/easy-medium/344-reverse-string.js
+++ leetcode/easy-medium/344-reverse-string.js
@@ -10,7 +10,16 @@ const t = ['H', 'a', 'n', 'n', 'a', 'h'];
 // Output: ["h","a","n","n","a","H"]
 
 const reverseString = function (s) {
-    return s.reverse();
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        [s[left], s[right]] = [s[right], s[left]];
+        left++;
+        right--;
+    }
+
+    return s;
 };
 
 console.log(reverseString(s));
