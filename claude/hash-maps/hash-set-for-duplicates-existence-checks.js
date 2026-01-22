f8b8f2db854fcfe56722eda32b695ec0148eb2ec
hallsarturo <proalarturo@gmail.com>
2026-01-22 12:22:56 -0600

    Refactor containsDuplicate function to use for-of loop for improved readability

--- claude/hash-maps/hash-set-for-duplicates-existence-checks.js
+++ claude/hash-maps/hash-set-for-duplicates-existence-checks.js
@@ -10,19 +10,19 @@
 // Given an integer array nums, return true if any value appears at least twice in the array,
 // and return false if every element is distinct.
 
-const nums = [1, 2, 3, 1];
+const nums = [1, 2, 3, ];
 // Output: true
 
 const containsDuplicate = function (nums) {
     const set = new Set();
 
-    nums.forEach((e) => {
-        if (set.has(e)) {
+    for (const num of nums) {
+        if (set.has(num)) {
             return true;
         } else {
-            set.add(e);
+            set.add(num);
         }
-    });
+    }
 
     return false;
 };
