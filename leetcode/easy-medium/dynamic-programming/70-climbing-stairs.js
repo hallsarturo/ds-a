9baa989c39aa962d3894e5a905e53db64056817b
hallsarturo <proalarturo@gmail.com>
2026-02-10 11:14:38 -0600

    Add initial implementation of house robber problem description

--- leetcode/easy-medium/dynamic-programming/70-climbing-stairs.js
+++ leetcode/easy-medium/dynamic-programming/70-climbing-stairs.js
@@ -1,4 +1,13 @@
 // You are climbing a staircase. It takes n steps to reach the top.
 // Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
+const climbStairs = function (n) {
+    if (n <= 2) return n;
+    const dp = [0, 1, 2];
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+};
 
+console.log(climbStairs(3));
