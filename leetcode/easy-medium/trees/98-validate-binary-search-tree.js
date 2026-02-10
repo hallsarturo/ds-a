9d3eca7d1d3fe74958f11778a441a3eb027beaa5
hallsarturo <proalarturo@gmail.com>
2026-02-09 19:45:50 -0600

    Implement isValidBST function and add test trees for validation

--- leetcode/easy-medium/trees/98-validate-binary-search-tree.js
+++ leetcode/easy-medium/trees/98-validate-binary-search-tree.js
@@ -6,6 +6,26 @@
 // The right subtree of a node contains only nodes with keys strictly greater than the node's key.
 // Both the left and right subtrees must also be binary search trees.
 
-const isValidBST = function(root) {
-    
+import { tree12, tree13, tree14, tree15 } from './dummy-tree-class.js';
+
+const isValidBST = function (root) {
+    const inorderArr = [];
+    inorderTraversal(root);
+    function inorderTraversal(current) {
+        if (current) {
+            inorderTraversal(current.left);
+            inorderArr.push(current.val);
+            inorderTraversal(current.right);
+        }
+    }
+
+    for (let i = 1; i < inorderArr.length; i++) {
+        if (inorderArr[i - 1] >= inorderArr[i]) return false;
+    }
+    return true;
 };
+
+console.log(isValidBST(tree12));
+console.log(isValidBST(tree13));
+console.log(isValidBST(tree14));
+console.log(isValidBST(tree15));
