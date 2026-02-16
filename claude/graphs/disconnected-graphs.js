db0ef6c862322ac63293d2f43266b2ca400424ee
hallsarturo <proalarturo@gmail.com>
2026-02-16 12:11:33 -0600

    Add depth-first search function to handle disconnected graphs

---
+++ claude/graphs/disconnected-graphs.js
@@ -0,0 +1,11 @@
+function dfsAll(graph) {
+    const visited = new Set();
+
+    for (let node in graph) {
+        if (!visited.has(node)) {
+            console.log(`New component starting at ${node}`);
+            dfs(graph, node, vis
+                ited);
+        }
+    }
+}
